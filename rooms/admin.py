99be41413df7e65d13c66b831e1b3ed34843379e
SonYongseok <qqfelix@naver.com>
2022-01-20 16:37:43 +0900

    #lec8.5 Photo Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -90,4 +91,11 @@ class RoomAdmin(admin.ModelAdmin):
 class PhotoAdmin(admin.ModelAdmin):
     """Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        # return f'<img source="{obj.file.url}'
+        # 위와 같이 실행시 동작하지 않음 -> django가 자동으로 해당 html을 배제 -> security
+        return mark_safe(f'<img width=50px src="{obj.file.url}"')
+
+    get_thumbnail.short_description = "Thumbnail"

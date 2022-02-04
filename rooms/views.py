8e05b684eb1c50e26e334ad5255492216788f069
SonYongseok <qqfelix@naver.com>
2022-02-04 14:45:15 +0900

    #lec13.2 Select Choices

--- rooms/views.py
+++ rooms/views.py
@@ -26,8 +26,21 @@ def search(request):
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
     room_types = models.RoomType.objects.all()
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+    }
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
     return render(
         request,
         "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
+        {**form, **choices},
     )

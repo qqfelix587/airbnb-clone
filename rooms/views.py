3248c5084a089e3d86c348f5cadbf347a6f82840
SonYongseok <qqfelix@naver.com>
2022-02-18 14:49:44 +0900

    #lec 23.4 Edit Photo

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from users import mixins as user_mixins
 from . import models, forms
 
@@ -159,3 +160,16 @@ def delete_photo(request, room_pk, photo_pk):
 
     except models.Room.DoesNotExist:
         return redirect(reverse("core:home"))
+
+
+class EditPhotoView(user_mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+    # https://ccbv.co.uk/projects/Django/4.0/django.views.generic.edit/UpdateView/    UpdateView
+    model = models.Photo
+    template_name = "rooms/photo_edit.html"
+    fields = ("caption",)
+    pk_url_kwarg = "photo_pk"
+    success_message = "Photo Updated."
+
+    def get_success_url(self):
+        room_pk = self.kwargs.get("room_pk")
+        return reverse("rooms:photos", kwargs={"pk": room_pk})
